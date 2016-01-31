9f55479cd34189514bce3323aeaeb25748593a3e
Andy Assareh <assareh@me.com>
2016-01-30 20:47:23 -0800

    Pylint updates

--- HNtoInstapaper.py
+++ HNtoInstapaper.py
@@ -1,7 +1,8 @@
-import instapaperlib
+import httplib
 import string
+import urlparse
+from instapaperlib import Instapaper
 import twitter
-import urllib, urllib2, urlparse, httplib
 
 CONSUMER_KEY = '***'
 CONSUMER_SECRET = '***'
@@ -12,61 +13,63 @@ INSTAPAPER_UN = '***'
 INSTAPAPER_PW = '***'
 
 HN_TWITTER = 'newsyc100'
-TARGET_URLS = ['newyorker.com','theatlantic.com']
+TARGET_URLS = ['newyorker.com', 'theatlantic.com']
 
 T_CO_SEARCH_KEY = 'https://t.co'
 
 class URLExpander: #https://taoofmac.com/space/blog/2009/08/10/2205
-  # known shortening services
-  shorteners = ['tr.im','is.gd','tinyurl.com','bit.ly','snipurl.com','cli.gs',
-                'feedproxy.google.com','feeds.arstechnica.com']
-  twofers = [u'\u272Adf.ws']
-  # learned hosts
-  learned = []
-    
-  def resolve(self, url, components):
-    """ Try to resolve a single URL """
-    c = httplib.HTTPConnection(components.netloc)
-    c.request("GET", components.path)
-    r = c.getresponse()
-    l = r.getheader('Location')
-    if l == None:
-      return url # it might be impossible to resolve, so best leave it as is
-    else:
-      return l
-  
-  def query(self, url, recurse = True):
-    """ Resolve a URL """
-    components = urlparse.urlparse(url)
-    # Check weird shortening services first
-    if (components.netloc in self.twofers) and recurse:
-      return self.query(self.resolve(url, components), False)
-    # Check known shortening services first
-    if components.netloc in self.shorteners:
-      return self.resolve(url, components)
-    # If we haven't seen this host before, ping it, just in case
-    if components.netloc not in self.learned:
-      ping = self.resolve(url, components)
-      if ping != url:
-        self.shorteners.append(components.netloc)
-        self.learned.append(components.netloc)
-        return ping
-    # The original URL was OK
-    return url
+    # known shortening services
+    shorteners = ['tr.im', 'is.gd', 'tinyurl.com', 'bit.ly', 'snipurl.com', 'cli.gs',
+                  'feedproxy.google.com', 'feeds.arstechnica.com']
+    twofers = [u'\u272Adf.ws']
+    # learned hosts
+    learned = []
 
-# Authenticate with Twitter API 
+    def resolve(self, url, components):
+        """ Try to resolve a single URL """
+        c = httplib.HTTPConnection(components.netloc)
+        c.request("GET", components.path)
+        r = c.getresponse()
+        l = r.getheader('Location')
+        if l == None:
+            return url # it might be impossible to resolve, so best leave it as is
+        else:
+            return l
+
+    def query(self, url, recurse=True):
+        """ Resolve a URL """
+        components = urlparse.urlparse(url)
+        # Check weird shortening services first
+        if (components.netloc in self.twofers) and recurse:
+            return self.query(self.resolve(url, components), False)
+        # Check known shortening services first
+        if components.netloc in self.shorteners:
+            return self.resolve(url, components)
+        # If we haven't seen this host before, ping it, just in case
+        if components.netloc not in self.learned:
+            ping = self.resolve(url, components)
+            if ping != url:
+                self.shorteners.append(components.netloc)
+                self.learned.append(components.netloc)
+                return ping
+        # The original URL was OK
+        return url
+
+# Authenticate with Twitter API
 api = twitter.Api(consumer_key=CONSUMER_KEY,
                   consumer_secret=CONSUMER_SECRET,
                   access_token_key=ACCESS_TOKEN_KEY,
                   access_token_secret=ACCESS_TOKEN_SECRET)
 
-# Pulls out the link URL from the tweet (link is first URL), and expands from t.co shortlink
 def getUrls(statuses):
+    """ Pulls out the link URL from the tweet (link is first URL), and expands from t.co 
+    shortlink """
     urls = []
     for s in statuses:
         if string.find(s.text, 'https://') > -1: # if it contains an https url
-            url = s.text[string.find(s.text, 'https://'):string.find(s.text, ' ', string.find(s.text, 'https://'))] # pull out the url
-            url_to_add = expander.query(url=url) 
+            url = s.text[string.find(s.text, 'https://'):string.find(s.text, ' ',\
+                         string.find(s.text, 'https://'))] # pull out the url
+            url_to_add = expander.query(url=url)
             if string.find(url_to_add, T_CO_SEARCH_KEY) == -1:
                 urls.append(url_to_add)
             else: # sometimes urls need expanding more than once
@@ -80,8 +83,8 @@ def getUrls(statuses):
 
 ##### should add a function for url expanding
 
-# Searches and filters list of URLs for target URL
 def filterUrls(urls):
+    """ Searches and filters list of URLs for target URL """
     filtered_urls = []
     for url in urls:
         for targetUrl in TARGET_URLS:
@@ -95,13 +98,12 @@ f.close()
 f = open(HN_TWITTER + '.txt', 'r+')
 last_id = f.readline()
 
-# GetUserTimeline(self, user_id=None, screen_name=None, since_id=None, max_id=None, 
+# GetUserTimeline(self, user_id=None, screen_name=None, since_id=None, max_id=None,
 #                 count=None, include_rts=True, trim_user=None, exclude_replies=None)
 statuses = api.GetUserTimeline(screen_name=HN_TWITTER, since_id=last_id, count=200)
 
 if len(statuses) > 0:
     # Instapaper Library
-    from instapaperlib import Instapaper
     i = Instapaper(INSTAPAPER_UN, INSTAPAPER_PW)
 
     expander = URLExpander()
@@ -110,11 +112,11 @@ if len(statuses) > 0:
     urls = filterUrls(urls)
 
     for url in urls:
-        print "added to Instapaper:",url
+        print "added to Instapaper:", url
         i.add_item(url, '')
 
     # Log latest tweet
-    f.seek(0,0)
+    f.seek(0, 0)
     f.write(str(statuses[0].id))
 
 else:
